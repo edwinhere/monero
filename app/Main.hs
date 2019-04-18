29007d5b4edc9a2b47db0f2eac65c89567960f28
Edwin Jose P <edwin.palathinkal@docdoc.com>
2019-04-18 16:58:55 +0800

    Optimized Imports.

--- app/Main.hs
+++ app/Main.hs
@@ -9,20 +9,24 @@ import Data.ByteArray (convert, ByteArray)
 import Crypto.ECC.Edwards25519
 import Crypto.Hash
 import Crypto.Error
-import Data.Serialize.Put
-import Data.Serialize.Get (
-        getWord64le,
-        runGet
-    )
-import Data.Serialize (encode)
-import Data.Word
 import Data.Modular
-import Data.Either
 
 type PrimeOrder = Integer/7237005577332262213973186563042994240857116359379907606001950938285454250989
 
 s2bs :: Scalar -> ByteString
 s2bs = scalarEncode
+
+scalarSub :: Scalar -> Scalar -> Scalar
+scalarSub a b = throwCryptoError . scalarDecodeLong . fromPrimeOrder $ a' - b'
+    where
+        a' :: PrimeOrder
+        a' = toPrimeOrder $ scalarEncode a
+        b' :: PrimeOrder
+        b' = toPrimeOrder $ scalarEncode b
+        toPrimeOrder :: ByteString -> PrimeOrder
+        toPrimeOrder = toMod . B.foldr (\ y x -> (256 * x) + (fromIntegral y)) 0
+        fromPrimeOrder :: PrimeOrder -> ByteString
+        fromPrimeOrder = B.unfoldr (\i' -> if i' == 0 then Nothing else Just (fromIntegral i', i' `div` 256)) . unMod
 
 main :: IO ()
 main = do
@@ -97,15 +101,3 @@ schnorrWithMessage m = do
           )
     print . B.unpack . s2bs $ lhs
     print . B.unpack . s2bs $ rhs
-
-scalarSub :: Scalar -> Scalar -> Scalar
-scalarSub a b = throwCryptoError . scalarDecodeLong . fromPrimeOrder $ a' - b'
-    where
-        a' :: PrimeOrder
-        a' = toPrimeOrder $ scalarEncode a
-        b' :: PrimeOrder
-        b' = toPrimeOrder $ scalarEncode b
-        toPrimeOrder :: ByteString -> PrimeOrder
-        toPrimeOrder = toMod . B.foldr (\ y x -> (256 * x) + (fromIntegral y)) 0
-        fromPrimeOrder :: PrimeOrder -> ByteString
-        fromPrimeOrder = B.unfoldr (\i' -> if i' == 0 then Nothing else Just (fromIntegral i', i' `div` 256)) . unMod
